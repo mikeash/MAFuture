952ef85fc901811d02ae5a2cdd85bbff609c2a2b
Ilya Kulakov <kulakov.ilya@gmail.com>
2010-08-19 13:57:03 +0700

    IKMemoryAwareFuture is a future for iPhone projects. It responds to the memory warnings and releases it's object from the memory. IKMemoryAwareFuture keeps block even after object is allocated.

--- MAFuture.h
+++ MAFuture.h
@@ -4,3 +4,15 @@ id MALazyFuture(id (^block)(void));
 
 #define MABackgroundFuture(...) ((__typeof((__VA_ARGS__)()))MABackgroundFuture((id (^)(void))(__VA_ARGS__)))
 #define MALazyFuture(...) ((__typeof((__VA_ARGS__)()))MALazyFuture((id (^)(void))(__VA_ARGS__)))
+
+
+#ifdef __IPHONE_4_0
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_4_0
+
+id IKMemoryAwareFuture(id (^block)(void));
+id IKMemoryAwareFutureCreate(id (^block)(void));
+#define IKMemoryAwareFuture(...)((__typeof((__VA_ARGS__)()))IKMemoryAwareFuture((id (^)(void))(__VA_ARGS__)))
+#define IKMemoryAwareFutureCreate(...)((__typeof((__VA_ARGS__)()))IKMemoryAwareFuture((id (^)(void))(__VA_ARGS__)))
+
+#endif // __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_4_0
+#endif // __IPHONE_4_0
